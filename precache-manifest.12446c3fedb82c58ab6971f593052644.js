c301ef161fe7b34522fd79188436b38571635e3e
Leonid Feskov <leonik89@gmail.com>
2019-10-09 00:25:03 +0300

    Updates

---
+++ precache-manifest.12446c3fedb82c58ab6971f593052644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65f782fdc1c89445d64c8cbfc3a5425",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ec70684302f0cbcc47d9",
+    "url": "/weather/static/css/main.8ba8d558.chunk.css"
+  },
+  {
+    "revision": "0764d237e3349cad15cb",
+    "url": "/weather/static/js/2.030b6064.chunk.js"
+  },
+  {
+    "revision": "ec70684302f0cbcc47d9",
+    "url": "/weather/static/js/main.8000c53a.chunk.js"
+  },
+  {
+    "revision": "a439d785d83b9852b72f",
+    "url": "/weather/static/js/runtime-main.2412ce67.js"
+  }
+]);
